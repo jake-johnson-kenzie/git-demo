f4b4d189cc5a5a8e23c653c10d5013dc1be9bb13
Jake Johnson <38015741+jakerjohnson94@users.noreply.github.com>
2021-07-06 13:11:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          can I break it this time?
         </p>
         <a
           className="App-link"
